d43f726c9081dd903bc0e2e785b8090d462bba17
bryce-phelps <bryce.phelps@indyimaging.com>
2016-07-19 09:59:42 -0400

    Little Clean up

--- public/js/services/MainService.js
+++ public/js/services/MainService.js
@@ -2,10 +2,6 @@
 // angular.module('MainService', []).factory('ApplicationObj', ['$http', function($http) {
 //
 //     return {
-//         // call to get all nerds
-//         get : function() {
-//             return $http.get('/api/nerds');
-//         },
 //
 //
 //                 // these will work when more API routes are defined on the Node side of things
@@ -14,10 +10,7 @@
 //             return $http.post('ApplicationDate', user);
 //         },
 //
-//         // call to DELETE a nerd
-//         delete : function(id) {
-//             return $http.delete('/api/nerds/' + id);
-//         }
+//         \
 //     }
 //
 // }]);
